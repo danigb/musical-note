38aef37ae8471f1c20f508f09d7db6dbe340a9f7
danigb <danigb@gmail.com>
2015-07-15 19:16:42 +0200

    npm package

---
+++ note.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = Note
+
+function Note(name) {
+  
+}
---
+++ test/note-test.js
@@ -0,0 +1,7 @@
+var vows = require('vows')
+var assert = require('assert')
+var Note = require('../')
+
+vows.describe('Note').addBatch({
+
+}).export(module)
